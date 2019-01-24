3cc9c136413e7be6b5716d97e594597bba4bb8a0
Colton Andrew Rinkenbe Severson <severs4@stolaf.edu>
2019-01-24 14:47:27 -0600

    updated the battery code to include LED

--- arduino experiments/battery/battery.ino
+++ arduino experiments/battery/battery.ino
@@ -1,23 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+float measuredvbat;
 #define VBATPIN A9
-   
-float measuredvbat = analogRead(VBATPIN);
-measuredvbat *= 2;    // we divided by 2, so multiply back
-measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
-measuredvbat /= 1024; // convert to voltage
-Serial.print("VBat: " ); Serial.println(measuredvbat);
+void setup() {
+    // put your setup code here, to run once:
+  pinMode(13, OUTPUT);//Red
+  pinMode(10, OUTPUT);//Green
+  pinMode(11, OUTPUT);//Blue
+  Serial.begin (9600);
+  measuredvbat = analogRead(VBATPIN);
+  measuredvbat *= 2;    // we divided by 2, so multiply back
+  measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024; // convert to voltage
+  Serial.print("VBat: " ); Serial.println(measuredvbat);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
-int.Counter=0;
-while (True){
-  Counter++;
-  if (Counter % 50 == 0)
-    if (measurevbat > 3.7)
-      Serial.print("Max");
-    else if (measurevbat <= 3.7) and (measurevbat >= 3.3)
-      Serial.print("Medium")
-    else if (measurevbat <3.3)
-      Serial.print("Empty")
-}}
+  int Counter=0;
+  while (true){
+    Counter++;
+    if (Counter % 50 == 0){
+      if (measuredvbat > 3.7){
+        Serial.println("Max");
+        analogWrite(13, 0);
+        analogWrite(10, 100);
+        analogWrite(11, 0);
+      }
+      else if (measuredvbat <= 3.7 && measuredvbat >= 3.3){
+        Serial.println("Medium");
+        analogWrite(13, 100);
+        analogWrite(10, 100);
+        analogWrite(11, 0);
+      }
+      else if (measuredvbat <3.3){
+        Serial.println("Empty");
+        analogWrite(13, 100);
+        analogWrite(10, 0);
+        analogWrite(11, 0);
+      }
+    }
+    delay(10);
+  }
+}

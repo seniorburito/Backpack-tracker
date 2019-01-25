e4ed6e18a53596f690cc7a40f798bd2555a03171
Roo Kosherbay <kosher1@stolaf.edu>
2019-01-25 10:17:50 -0600

    add proximity sound

--- buttons_feather/buttons_feather.ino
+++ buttons_feather/buttons_feather.ino
@@ -1,23 +1,67 @@
-const int buttonPin = 2;     // the number of the pushbutton pin
+
+
+const int buttonPin = 2;
 const int buttonPin1 = 3;
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+const int powerPin = 5;
+const int speakerPin = 10;
+const int potentiometerPin = A1; 
+
+
+int buttonState = 0;
 int buttonState1 = 0;
+int wait;
+int potentiometerValue = 0;
+  
+bool on = false;
+
 void setup() {
   // initialize the LED pin as an output:
   Serial.begin(115200);
-  // initialize the pushbutton pin as an input:
+  // initialize the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
   pinMode(buttonPin1, INPUT);
+  pinMode(powerPin, OUTPUT);
+  pinMode(speakerPin, OUTPUT);
+  digitalWrite(powerPin, HIGH);
 }
 
 void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   buttonState1 = digitalRead(buttonPin1);
+  potentiometerValue = analogRead(potentiometerPin);
+  Serial.println(potentiometerValue);
+  Serial.println("######################################");
+  if(buttonState){
+    on = !(on);
+    Serial.println("yo");
+  }
+  
+  /*
   Serial.print("button is: ");
   Serial.print(buttonState);
   Serial.print("          button1 is: ");
   Serial.println(buttonState1);
+  */
   delay(10);
+  while(on == true){
+    potentiometerValue = analogRead(potentiometerPin);
+    wait = map(potentiometerValue, 0, 1023, 50, 1000);
+    tone(speakerPin, 392);
+    delay(wait);
+    buttonState = digitalRead(buttonPin);
+    if(buttonState){
+      on = !(on);
+      noTone(speakerPin);
+      break;
+    }
+    noTone(speakerPin);
+    delay(wait);
+    buttonState = digitalRead(buttonPin);
+    if(buttonState){
+      on = !(on);
+      break;
+    }
+  }
+  delay(50);
 }
